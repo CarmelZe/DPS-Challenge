9b04ea56f3bc3e4d6979e8d00b2f0bde392cbab1
Carmel <carmel.zerihun@gmail.com>
2023-11-25 17:31:41 +0300

    error fixed

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ st.title('Predictor App')
 
 # Input form
 st.header('Input')
-year = st.number_input('Year', min_value=2000, max_value=2021, step=1)
-month = st.number_input('Month', min_value=1, max_value=12, step=1)
+Year = st.number_input('Year', min_value=2000, max_value=2021, step=1)
+Month = st.number_input('Month', min_value=1, max_value=12, step=1)
 
 # Prediction
 if st.button('Predict'):
-    input_data = {'year': year, 'month': month}
+    input_data = {'Year': Year, 'Month': Month}
     input_df = pd.DataFrame([input_data])
     prediction = model.predict(input_df)[0]
     st.success(f'The predicted value is: {prediction}')
